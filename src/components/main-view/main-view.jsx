f19dc575aabbd82bebb0c11d74137f51bb55e7a2
Guido43 <gbsdpiano@yahoo.co.uk>
2023-08-08 14:15:22 +0100

    updates made to prop-types

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,11 +1,8 @@
 import { useState } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
-<<<<<<< Updated upstream
-import { useState } from "react";
-=======
 import { useEffect } from "react";
->>>>>>> Stashed changes
+
 
 export const MainView = () => {
   const [movies, setMovies] = useState([]);
@@ -19,25 +16,14 @@ export const MainView = () => {
           return {
             id: doc.key,
             title: doc.title,
-<<<<<<< Updated upstream
             image: `https://covers.openlibrary.org/b/id/${doc.cover_i}-L.jpg`,
             director: doc.director_name?.[0]
           };
         });
+        
         setMovies(moviesFromApi);
       });
-  }, []);
-=======
-            image: "https://www.alamy.com/stock-photo-the-mask-1994-entertainmentnew-line-film-with-jim-carrey-28304904.html?imageid=9707B241-9983-443A-B1BF-4DD663E5983D&p=13044&pn=1&searchId=57bc1de0872727742c879d76d51c047a&searchtype=0",
-            director: doc.author_name?.[0]
-          };
-        });
-
-        setMovies(moviesFromApi);
-      });
-  }, []);
-
->>>>>>> Stashed changes
+  }, []);          
 
   if (selectedMovie) {
     return (
