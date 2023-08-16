db9ac80575fd85e0c920ffb0ab693450666bdc65
Guido43 <gbsdpiano@yahoo.co.uk>
2023-08-16 21:38:27 +0100

    login-view file created

---
+++ src/components/login-view/login-view.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import { React } from "react";
+
+
+export const LoginView = ({ onLoggedIn }) => {
+       const [username, setUsername] = useState("");
+       const [password, setPassword] = useState("");
+
+       const handleSubmit = (event) => {
+        event.preventDefault();
+
+       const data = {
+          username: username,
+          password: password
+        };
+                    
+      fetch("https://guysflix-d8285acb1f18.herokuapp.com/login",
+      {method: "POST",
+      headers: {"content-type":"application/json"
+    },
+       body:JSON.stringify(data)
+      }).then ((response) => response.json())
+      .then ((data) => {
+        console.log("login response:", data);
+        if (data.user) {
+          localStorage.setItem("user", JSON.stringify(data.user));
+          localStorage.setItem("token", data.token);
+          onLoggedIn (data.user, data.token);
+        } else
+      {alert("no such user");
+    }
+  })
+  .catch((e) => {alert("something went wrong");
+});
+}
+
+
+    return (
+      <form onSubmit = {handleSubmit}>
+        <label>
+          Username:
+          <input type="text" 
+           value={username} 
+           onChange={(e) => setUsername(e.target.value)} required />
+        </label>
+        <label>
+          Password:
+          <input type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)} required />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    );
+  };
