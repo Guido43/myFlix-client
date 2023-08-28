4e172cd2ffc2f6259003ebc23a5fb8eff065642a
Guido43 <gbsdpiano@yahoo.co.uk>
2023-08-28 21:57:37 +0100

    movie card now using react-bootstrap card and button

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,20 +1,20 @@
 import PropTypes from "prop-types";
+import { Button, Card } from "react-bootstrap";
 
 
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
-    <div
-      onClick={() => {
-        onMovieClick(movie);
-      }}
-    >
-      <div>
-      {movie.Title}</div>
-   
-    
+    <Card className="h-100">
+      <Card.Img variant="top" src={movie.imagePath} />
+      <Card.Body>
+        <Card.Title>{movie.Title}</Card.Title>
+        <Card.Text>{movie.Description}</Card.Text>
         
-
-    </div>
+        <Button onClick={() => onMovieClick(movie)} variant="link">Open</Button>
+      </Card.Body>
+      </Card>
+    
+    
   );
 };
 MovieCard.propTypes = {
