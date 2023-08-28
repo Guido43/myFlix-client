60805eaafe319d8c682c7bd442685df7aab8e773
Guido43 <gbsdpiano@yahoo.co.uk>
2023-08-28 21:54:55 +0100

    Packages updated for bootstrap and react-bootstrap

--- src/components/signup-view/signup-view.jsx
+++ src/components/signup-view/signup-view.jsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import { React } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
 
 export const SignupView = ({ onSignedIn }) => {
   const [username, setUsername] = useState("");
@@ -37,46 +40,46 @@ export const SignupView = ({ onSignedIn }) => {
   }
     
       return (
-        <form onSubmit={handleSubmit}>
-          <label>
-            Username:
-            <input
-              type="text"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              required
-              minLength="3"
-            />
-          </label>
-          <label>
-            Password:
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </label>
-          <label>
-            Email:
-            <input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </label>
-          <label>
-            Birthday:
-            <input
-              type="date"
-              value={birthday}
-              onChange={(e) => setBirthday(e.target.value)}
-              required
-            />
-          </label>
-          <button type="submit">Submit</button>
-        </form>
+      <Form onSubmit = {handleSubmit}>
+        <Form.Group controlId="formUserName">
+          <Form.Label>Username:</Form.Label>
+          <Form.Control
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            requiredminLength="3"/>
+        </Form.Group>
+
+        <Form.Group controlId="formPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required />
+          </Form.Group>
+          
+        <Form.Group controlId="formEmail">
+          <Form.Label>Email:</Form.Label>
+          <Form.Control
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            requiredminLength="3"/>
+      </Form.Group>
+
+      <Form.Group controlId="formBirthday">
+        <Form.Label>Date of Birth</Form.Label>
+        <Form.Control
+          type="date"
+          value={birthday}
+          onChange={(e) => setBirthday(e.target.value)}
+          required />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+    </Form>   
       );
     };
   
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,14 @@
+//import "bootstrap/dist/css/bootstrap.min.css"
 import { createRoot } from 'react-dom/client';
 import { MainView } from './components/main-view/main-view';
-import "./index.scss"
+import "./index.scss";
+import Container from "react-bootstrap/Container"
 
 // Main component (will eventually use all the others)
 const MyFlixApplication = () => {
-  return <MainView></MainView>;
+  return <Container>
+    <MainView></MainView>
+    </Container>;
 };
 
 // Finds the root of your app
