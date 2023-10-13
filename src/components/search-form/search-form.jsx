de1371e0ef924025274d050b69ddef55184b0963
Guido43 <gbsdpiano@yahoo.co.uk>
2023-10-13 13:12:35 +0200

    created search bar for movie filter

---
+++ src/components/search-form/search-form.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { Form, FormControl } from 'react-bootstrap';
+import "./search-form.scss";
+
+export const SearchForm = ({ onSearch }) => {
+    const [searchName, setSearchName] = useState('');
+
+    const handleSearchItem = (e) => {
+        const newSearchName = e.target.value;
+        setSearchName(newSearchName);
+
+            if (newSearchName === '') {
+            onSearch('');
+            } else {
+                onSearch(newSearchName);
+            }
+    };
+
+    return (
+        <Form>
+            <FormControl
+                className="mb-sm-2"
+                id="search-bar"
+                type="text"
+                placeholder="Search movies by title..."
+                value={searchName}
+                onChange={handleSearchItem}
+                />
+        </Form>
+    );
+}
