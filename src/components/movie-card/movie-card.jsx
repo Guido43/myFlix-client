23c8d46497383394e1306483f01dcabc83e77aec
Guido43 <gbsdpiano@yahoo.co.uk>
2023-10-13 13:06:51 +0200

    funtional change for image display

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -3,18 +3,19 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import "./movie-card.scss";
 
 
-export const MovieCard = ({ movie, user, setUser, token }) => {
+export const MovieCard = ({ movie, user, setUser, token, imagePath }) => {
   return (
     <Card className="h-100">
-      <Card.Img variant="top" src={movie.imagePath} />
+      <Card.Img variant="top" className="movie-image" src={movie.imagePath} />
       <Card.Body>
         <Card.Title>{movie.Title}</Card.Title>
         <Card.Text>{movie.Description}</Card.Text>
 
         <Link to={`/movies/${encodeURIComponent(movie._id)}`} state={{ user: user }}>
-        <Button variant="link">Open</Button>
+        <Button variant="link">more information</Button>
         </Link>
       </Card.Body>
       </Card>
@@ -22,7 +23,7 @@ export const MovieCard = ({ movie, user, setUser, token }) => {
     
   );
 };
-MovieCard.propTypes = {
+MovieCard.PropTypes = {
   movie: PropTypes.shape({
     Title: PropTypes.string.isRequired,
     imagePath: PropTypes.string.isRequired,
